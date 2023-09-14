44dce17f26c319b867a4b5d918a70319df8e3fcb
John Coleman <jbcolemanjr@gmail.com>
2023-09-13 21:31:26 -0400

    complete state and events lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false)
+  const cartStatus = isInCart ? "in-cart" : ""
+  const cartClick = (e) => setIsInCart((isInCart) => !isInCart)
+
+
+
   return (
-    <li className="">
+    <li className={cartStatus}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={cartClick} className="add">{isInCart ? "Add Item" : "Remove Item"}</button>
     </li>
   );
 }
